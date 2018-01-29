c8b35ea57e4faad7fbc1e7ac1495d9566325c69e
Yizhi Yin <yizhiyin@wireless-10-145-151-101.public.utexas.edu>
2018-01-29 16:38:31 -0600

    Update the main.py finished code except writing into text

---
+++ PyBank/main.py
@@ -0,0 +1,77 @@
+
+#Part I 
+# ______________________________________________________________
+# import csv and convert csv into a list called csv_list_no_header
+#________________________________________________________________
+import csv
+import os
+csvpath=os.path.join("raw_data", "budget_data_1.csv")
+csv_list=[]
+with open(csvpath,newline="") as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    for row in csvreader:
+        csv_list.append(row)
+csv_list_no_header=csv_list[1:]
+
+#Part II
+# ____________________________________________________________ 
+# calculate the number of months as well as the total revenue
+# ____________________________________________________________
+num_of_month=len(csv_list_no_header)
+
+#Calculate total revenue
+total_revenue=0
+for ele in csv_list_no_header:
+    total_revenue+=int(ele[1])
+# # print("debug "+total_revenue)
+
+#Part III 
+#__________________________________________________________
+# create another list to store change of revenue by month 
+#this list is named as change_by_month_list
+#__________________________________________________________
+change_by_month_list=[]
+i=1
+while i<=(len(csv_list_no_header)-1):
+    change_by_month=int(csv_list_no_header[i][1])-int(csv_list_no_header[i-1][1])
+    change_by_month_list.append(change_by_month)
+    i+=1
+# # print("debug "+len(change_by_month_list))
+# # print("debug "+change_by_month_list[-1])
+#
+# PartIV
+# ________________________________________________________________
+# calculate the average_revenue_change
+# find the greatest changes of revenue in the change_by_month_list
+# and the index number of the greatest changes of the revenue
+# use the index number of the greatest increase and decrease to 
+# access the corresponding date from the csv_list_no_header
+#________________________________________________________________
+
+#Calculate the average of revenue change by month
+average_revenue_change=sum(change_by_month_list)/len(change_by_month_list)
+
+#find the greatest increase revenue and its index number
+greatest_increase=max(change_by_month_list)
+index_increase=change_by_month_list.index(greatest_increase)
+
+# find the greatest decrease of revenue and its index number
+greatest_decrease=min(change_by_month_list)
+index_decrease=change_by_month_list.index(greatest_decrease)
+
+# # print("debug {},{},{},{}".format(greatest_increase,index_increase,greatest_decrease,index_decrease))
+
+#retrieve the corresponding dates with greatest increase or decrease from 
+#csv_list_no_header
+Date_greatest_increase=csv_list_no_header[index_increase+1][0]
+Date_greatest_decrease=csv_list_no_header[index_decrease+1][0]
+# # print("debug "+Date_greatest_increase)
+# # print("debug "+Date_greatest_decrease)
+
+print("\nFinancial Analysis \n"+"__________________________________________\n"
+    "Total months: "+str(num_of_month)+"\n"
+    "Total Revenue: $"+str(total_revenue)+"\n"
+    "Average Revenue Change: $"+str(average_revenue_change)+"\n"
+    "Greatest Increase in Revenue: "+str(Date_greatest_increase) + " ($" + str(greatest_increase) +")\n"
+    "Greatest Decrease in Revenue: "+str(Date_greatest_decrease) + " ($" + str(greatest_decrease) +")\n")
+
